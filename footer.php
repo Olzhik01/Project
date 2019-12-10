9e17f897a63c2c666c0f0a02f8eda38d3be944a4
Olzhik01 <55508244+Olzhik01@users.noreply.github.com>
2019-12-10 07:34:44 +0600

    Create footer.php
    It is a footer of our site

---
+++ footer.php
@@ -0,0 +1,84 @@
+<div class="footer">
+		<div class="useful_links">
+			
+			<span id="useful_links_logo">Useful links</span>
+			
+			<div class="links">
+				
+				<ul>
+					<li><a href="#">Login</a></li>
+					<li><a href="#">Post Book</a></li>
+					<li><a href="#">What to read?</a></li>
+					<li><a href="#">About us</a></li>
+					<li><a href="#">Favorites</a></li>
+				</ul>
+			</div>
+
+		</div>
+
+		<div class="contacts">
+
+			
+			
+			<div class="get_in_touch">
+				<span id="contacts_logo">Contacts</span>
+				<scan id="location"><i id="location_icon" class="fa fa-map-marker"></i> SDU,Kaskelen,Almaty,KZ</scan>
+
+				<scan id="phone"><i id="phone_icon" class="fa fa-mobile-phone"></i> Telephone: +77024869876 </scan>
+
+				<scan id="fax"><i id="faxIcon" class="fa fa-fax"></i> FAX: +1809 876</scan>
+				<scan id="email"><i id="emailIcon" class="fa fa-envelope"></i> Email: <a id="stu" href="#"> 180103273@stu.sdu.edu.kz</a></scan>
+				
+
+			</div>
+			
+		</div>
+
+
+		<div class="follow_us">
+			
+			
+			<div class="follow_logo">
+			
+			<span id="follow_word">Follow Me</span>
+				
+				<div class="facebook">
+					<a href="https://vk.com/id307214735" target="_blank"><i id="facebook_icon" class="fa fa-facebook"></i></a>
+				</div>
+				
+				<div class="twitter">
+					<a href="https://vk.com/id307214735" target="_blank"><i id="twitter_icon" class="fa fa-twitter"></i></a>
+				</div>
+				
+				<div class="google">
+					<a href="https://vk.com/id307214735" target="_blank"><i id="google_icon" class="fa fa-google-plus"></i></a>
+				</div>
+
+				<div class="vk">
+					<a href="https://vk.com/id307214735" target="_blank"><i id="vk_icon" class="fa fa-vk"></i></a>
+				</div>
+
+
+				<div class="instagram">
+					<a href="https://vk.com/id307214735" target="_blank"><i id="insta_icon" class="fa fa-instagram"></i></a>
+				</div>
+
+
+				<div class="github">
+					<a href="https://github.com/Olzhik01/Project" target="_blank"><i id="git_icon" class="fa fa-github"></i></a>
+				</div>
+
+			</div>
+
+		</div>
+		
+		<div class="rights">
+			<p>All rights reserved &copy; <?=date('Y')?></p>
+			
+		</div>
+		
+
+
+			
+
+	</div>
