8dc0c6618be000ab6a37139c6b9569cde5cd82f1
Olzhik01 <55508244+Olzhik01@users.noreply.github.com>
2019-12-10 14:21:23 +0600

    Update index.php

--- index.php
+++ index.php
@@ -28,8 +28,167 @@
 	</style>
 </head>
 <body>
-	<?php require_once "blocks/header.php" ?>
+	
+<?php require_once "blocks/header.php" ?>
 
+	<div class="main_page_categories">
+		
+		<div class="categories_list">
+			
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div id="">
+				<span id="forKids">Детям</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Детективы</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Ужасы</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Фэнтези и фантастика</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Драмы</span>
+				</div>
+			</a>
+
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Биография</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Поэзия</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div >
+				<span id="forKids">Юмор и сатира</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Военная литература</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div >
+				<span id="forKids">Романы</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Триллеры</span>
+				</div>
+			</a>
+
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Духовная литература</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Научно-Образовательная</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Боевики</span>
+				</div>
+			</a>
+
+			<a href="" class="link_kids">
+				<div class="category_kids">
+					<img src="/img/1.jpeg" alt="for kids">
+				</div>
+
+				<div>
+				<span id="forKids">Приключения</span>
+				</div>
+			</a>
+		</div>
+		
+	</div>
 
 	<?php require_once "blocks/footer.php" ?>
 
