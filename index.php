828edbadcea45a60f765cc92608d4d69130fb04a
Olzhik01 <55508244+Olzhik01@users.noreply.github.com>
2019-12-10 07:36:33 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>FindBook</title>
-<style type="text/css">
-	body{
-		margin:0; 
-	}
-</style>
+	<?php 
+		$title="Главная страница";
+		require_once "blocks/head.php"; 
+	?>
+
+	<style type="text/css">
+		
+		@media screen and (max-width: 200px){
+			#logo{
+				position: absolute;
+				top:10px;
+				left:20px;
+			}
+			
+
+			.header_links {
+				position: absolute;
+				left:10px;
+				top:40px;
+				
+				
+			}
+		}	
+		
+	</style>
 </head>
 <body>
-	
+	<?php require_once "blocks/header.php" ?>
+
+
+	<?php require_once "blocks/footer.php" ?>
+
+
+
 </body>
-</html>	
+</html>
