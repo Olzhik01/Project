d84308858c52b117a0a64939374c5d5f66701237
Olzhik01 <55508244+Olzhik01@users.noreply.github.com>
2019-12-20 19:32:09 +0600

    Create favorites.php
    Favorite section

---
+++ favorites.php
@@ -0,0 +1,29 @@
+.form-exit{
+			background-color: white;
+			margin: 20px;
+			display: grid;
+			justify-content: center;
+			grid-template-columns: repeat(auto-fit,minmax(200px, 1fr)); 
+
+			grid-template-rows: repeat(auto-fit,minmax(500px,1fr));
+
+		}
+
+		.tab-exit{
+			margin: 20px;
+			padding: 20px;
+			border: 2px solid #FF8C00;
+			display: grid;
+
+			grid-template-columns: repeat(2,minmax(100px, 1fr));
+			grid-template-rows: 50px;
+			justify-self: center;
+
+			grid-gap: 20px;
+
+
+		}
+		#exit{
+			color: orange;
+			border-bottom: 1px solid orange;
+		}
